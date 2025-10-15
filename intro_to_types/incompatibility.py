5788f2548796b37d031c7d6e423fa13714ed598c
Michael Berrios <mxbics@rit.edu>
2025-10-15 17:53:47 -0400

    Completed Type Casting Demo

---
+++ intro_to_types/incompatibility.py
@@ -0,0 +1,3 @@
+# print("Hello, I am " + 18 + " Years old")
+print("Hello, I am " + str(18) + " Years old")
+# print("Hello, I am", 18, "Years old")
---
+++ intro_to_types/sum.py
@@ -0,0 +1,13 @@
+def sum(num1, num2):
+    print(type(num1))
+    print(type(num2))
+    return num1 + num2
+
+            # collects a string
+num1_input = int(input("Enter num 1: "))
+
+            # collects a string
+num2_input = int(input("Enter num 2: "))
+
+result = sum(num1_input, num2_input)
+print("Results: ", result)
